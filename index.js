e005f2631939b6991a42652d43e2039d858f957b
mukesh <mukesh@halsimplify.com>
2022-03-14 22:55:11 +0530

    add client

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const exp = require('constants');
 const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
 
 //create express app
 const app = express();
@@ -8,8 +11,10 @@ const port = process.env.PORT || 3000;
 
 //define roote route
 app.get('/', (req, res)=>{
-    res.send("Hello Express Js");
+    res.sendFile(path.join(__dirname, 'Client/front-end', 'home.html'));
 });
+
+// app.use(express.static(path.join(__dirname, 'Client/front-end', 'home.html')));
 
 const StudentRoutes = require('./src/routes/student.router');
 
